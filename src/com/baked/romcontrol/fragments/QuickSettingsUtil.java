78c6acc19fcfbc1a1334e74f49baf40f141179cc
travp624 <travp624@gmail.com>
2013-01-25 00:12:12 -0600

    QuickSettings: add LTE tile (2/2)
    Signed-off-by: travp624 <travp624@gmail.com>

--- src/com/baked/romcontrol/fragments/QuickSettingsUtil.java
+++ src/com/baked/romcontrol/fragments/QuickSettingsUtil.java
@@ -103,6 +103,12 @@ public class QuickSettingsUtil {
         TILES.put(TILE_LOCKSCREEN, new QuickSettingsUtil.TileInfo(
                 TILE_LOCKSCREEN, R.string.title_tile_lockscreen,
                 "com.android.systemui:drawable/ic_qs_lock_screen_off"));
+        if (PhoneConstants.LTE_ON_CDMA_TRUE == TelephonyManager.getDefault().getLteOnCdmaMode() ||
+                    TelephonyManager.getDefault().getLteOnGsmMode() != 0) {
+            TILES.put(TILE_LTE, new QuickSettingsUtil.TileInfo(
+                    TILE_LTE, R.string.title_tile_lte,
+                    "com.android.systemui:drawable/ic_qs_lte_off"));
+        }
         TILES.put(TILE_MOBILEDATA, new QuickSettingsUtil.TileInfo(
                 TILE_MOBILEDATA, R.string.title_tile_mobiledata,
                 "com.android.systemui:drawable/ic_qs_signal_4"));
@@ -142,14 +148,6 @@ public class QuickSettingsUtil {
         TILES.put(TILE_USER, new QuickSettingsUtil.TileInfo(
                 TILE_USER, R.string.title_tile_user,
                 "com.android.systemui:drawable/ic_qs_default_user"));
-
-// These toggles are not available yet.  Comment out for now
-//        if(PhoneConstants.LTE_ON_CDMA_TRUE == TelephonyManager.getDefault().getLteOnCdmaMode() ||
-//           TelephonyManager.getDefault().getLteOnGsmMode() != 0) {
-//            TILES.put(TILE_LTE, new QuickSettingsUtil.TileInfo(
-//                    TILE_LTE, R.string.title_tile_lte,
-//                    "com.android.systemui:drawable/stat_lte_on"));
-//        }
 //        TILES.put(TILE_WIMAX, new QuickSettingsUtil.TileInfo(
 //                TILE_WIMAX, R.string.title_tile_wimax,
 //                "com.android.systemui:drawable/stat_wimax_on"));
