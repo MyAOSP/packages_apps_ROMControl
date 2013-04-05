54d413c060eeba1b35903755ff60c339c87b8c29
travp624 <travp624@gmail.com>
2013-04-05 16:02:51 -0500

    Expanded desktop fix. Also only show checkbox for devices without navbar by default.
    Signed-off-by: travp624 <travp624@gmail.com>

--- src/com/baked/romcontrol/fragments/PowerMenu.java
+++ src/com/baked/romcontrol/fragments/PowerMenu.java
@@ -3,6 +3,7 @@ package com.baked.romcontrol.fragments;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -10,17 +11,21 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
 
 import com.baked.romcontrol.R;
 import com.baked.romcontrol.BAKEDPreferenceFragment;
 
 public class PowerMenu extends BAKEDPreferenceFragment implements
             OnPreferenceChangeListener {
+    private static final String TAG = "PowerMenu";
 
     private static final String PREF_SCREENSHOT = "show_screenshot";
     private static final String PREF_POWER_OFF = "show_power_off";
     private static final String PREF_REBOOT_CHOOSER = "show_reboot_chooser";
     private static final String PREF_EXPANDED_DESKTOP = "expanded_desktop";
+    private static final String PREF_EXPANDED_DESKTOP_NO_NAVBAR = "expanded_desktop_no_navbar";
     private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
     private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
     private static final String PREF_SHOW_PROFILE_CHOOSER = "show_profile_chooser";
@@ -33,6 +38,7 @@ public class PowerMenu extends BAKEDPreferenceFragment implements
     CheckBoxPreference mShowNavBarHide;
     CheckBoxPreference mShowProfileChooser;
     CheckBoxPreference mShowSoundChooser;
+    CheckBoxPreference mExpandedDesktopNoNavbar;
     ListPreference mExpandedDesktop;
 
     @Override
@@ -49,6 +55,23 @@ public class PowerMenu extends BAKEDPreferenceFragment implements
         mShowNavBarHide = (CheckBoxPreference) findPreference(PREF_NAVBAR_HIDE);
         mShowProfileChooser = (CheckBoxPreference) findPreference(PREF_SHOW_PROFILE_CHOOSER);
         mShowSoundChooser = (CheckBoxPreference) findPreference(PREF_SHOW_SOUND_CHOOSER);
+        mExpandedDesktopNoNavbar = (CheckBoxPreference) findPreference(PREF_EXPANDED_DESKTOP_NO_NAVBAR);
+
+        // Switch for expanded desktop when theres no navbar.
+        int exDeskVal = Settings.System.getInt(mContentResolver,
+                Settings.System.EXPANDED_DESKTOP_STYLE, 0);
+        try {
+            if (WindowManagerGlobal.getWindowManagerService().hasNavigationBar()) {
+                mExpandedDesktop.setValue(String.valueOf(exDeskVal));
+                updateExpandedDesktop(exDeskVal);
+                prefSet.removePreference(mExpandedDesktopNoNavbar);
+            } else {
+                mExpandedDesktopNoNavbar.setChecked(exDeskVal > 0);
+                prefSet.removePreference(mExpandedDesktop);
+            }
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error getting navigation bar status");
+        }
     }
 
     @Override
@@ -65,8 +88,7 @@ public class PowerMenu extends BAKEDPreferenceFragment implements
             int index = mExpandedDesktop.findIndexOfValue((String) newValue);
             preference.setSummary(mExpandedDesktop.getEntries()[index]);
             int value = Integer.valueOf((String) newValue);
-            Settings.System.putInt(mContentResolver,
-                    Settings.System.EXPANDED_DESKTOP_STYLE, value);
+            updateExpandedDesktop(value);
             return true;
         }
         return false;
@@ -102,12 +124,18 @@ public class PowerMenu extends BAKEDPreferenceFragment implements
             Settings.System.putInt(mContentResolver, Settings.System.POWER_DIALOG_SHOW_SOUND_CHOOSER,
                     checkBoxChecked(preference) ? 1 : 0);
             return true;
+        } else if (preference == mExpandedDesktopNoNavbar) {
+            boolean val = mExpandedDesktopNoNavbar.isChecked();
+            updateExpandedDesktop(val ? 2 : 0);
+            return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     private void registerListeners() {
-        mExpandedDesktop.setOnPreferenceChangeListener(this);
+        if (mExpandedDesktop != null) {
+            mExpandedDesktop.setOnPreferenceChangeListener(this);
+        }
     }
 
     private void setDefaultValues() {
@@ -125,11 +153,31 @@ public class PowerMenu extends BAKEDPreferenceFragment implements
                 Settings.System.POWER_DIALOG_SHOW_PROFILE_CHOOSER, 0) == 1);
         mShowSoundChooser.setChecked(Settings.System.getInt(mContentResolver,
                 Settings.System.POWER_DIALOG_SHOW_SOUND_CHOOSER, 1) == 1);
-        mExpandedDesktop.setValue(Integer.toString(Settings.System.getInt(mContentResolver,
-                Settings.System.EXPANDED_DESKTOP_STYLE, 0)));
     }
 
     private void updateSummaries() {
-        mExpandedDesktop.setSummary(mExpandedDesktop.getEntry());
+        if (mExpandedDesktop != null) {
+            mExpandedDesktop.setSummary(mExpandedDesktop.getEntry());
+        }
+    }
+
+    private void updateExpandedDesktop(int value) {
+        Settings.System.putInt(mContentResolver,
+                Settings.System.EXPANDED_DESKTOP_STYLE, value);
+
+        if (value == 0) {
+            // Expanded desktop deactivated
+            Settings.System.putInt(mContentResolver,
+                    Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE, 0);
+            // Disable expanded desktop if enabled
+            Settings.System.putInt(mContentResolver,
+                    Settings.System.EXPANDED_DESKTOP_STATE, 0);
+        } else if (value == 1) {
+            Settings.System.putInt(mContentResolver,
+                    Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE, 1);
+        } else if (value == 2) {
+            Settings.System.putInt(mContentResolver,
+                    Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE, 1);
+        }
     }
 }
